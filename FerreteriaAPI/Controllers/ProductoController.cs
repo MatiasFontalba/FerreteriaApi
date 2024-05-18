f42f566ff76de1814a8477f682e75fe5be966cf1
MatiasFontalba <ma.fontalba@duocuc.cl>
2024-05-18 04:03:28 -0400

    creacion de clase y controlador producto

---
+++ FerreteriaAPI/Controllers/ProductoController.cs
@@ -0,0 +1,47 @@
+﻿using FerreteriaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FerreteriaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        static IList<Producto> lista = new List<Producto>();
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IList<Producto> Get()
+        {
+            return lista;
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(int id)
+        {
+            return lista.FirstOrDefault(x => x.idProducto == id);
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] Producto objeto)
+        {
+            lista.Add(objeto);
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            lista.Remove(lista.FirstOrDefault(x => x.idProducto == id));
+        }
+    }
+}
---
+++ FerreteriaAPI/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿namespace FerreteriaAPI.Models
+{
+    public class Producto
+    {
+        public int idProducto { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string imagen { get; set; }
+
+        public Producto()
+        {
+            this.idProducto = new int();
+            this.nombre = string.Empty;
+            this.descripcion = string.Empty;
+            this.imagen = string.Empty;
+        }
+    }
+}
